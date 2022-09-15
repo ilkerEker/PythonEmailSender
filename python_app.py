2263bd83816bba6f5f8e8eeb2b260dd8ec6fc684
Ilker Eker <lkrkr@hotmail.com>
2022-09-15 13:10:42 +0300

    2. sample

--- python_app.py
+++ python_app.py
@@ -1,10 +1,9 @@
 from email.message import EmailMessage
-from multiprocessing import context
 from app2 import password
 import ssl
 import smtplib
   
-email_sender='codewithtomi@gmail.com'
+email_sender='lkrkr06@gmail.com'
 email_password=password
 
 email_receiver='ilker.eker@outlook.com.tr'
@@ -22,6 +21,6 @@ em.set_content(body)
 
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL('smtpçgmail.com',465,context=context) as smtp:
+with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
     smtp.login(email_sender,email_password)
     smtp.sendmail(email_sender,email_receiver,em.as_string())
---
+++ word_replacement.py
@@ -0,0 +1,9 @@
+def replace_word():
+    str="Hi Guys, i am tomi, and hi hi hi hi"
+    word_to_replace=input("ENter the word to replace:")
+    word_replacement=input("ENter the word to replacement:")
+    print(str.replace(word_to_replace,word_replacement))
+
+
+replace_word()
+
